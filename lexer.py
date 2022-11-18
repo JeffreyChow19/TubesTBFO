94bf4985acadac39f2a6c4827be723c733454ab1
Jeffrey Chow <16521353@mahasiswa.itb.ac.id>
2022-11-18 09:02:40 +0700

    feat: CYK

--- lexer.py
+++ lexer.py
@@ -3,11 +3,11 @@ import sys
 
 tokenCollection = [
     # Tuples for defining JS syntax to GRAMMAR
-    
+
     # IGNORE TYPES
-    (r'[ \t]+',                        None), # ignore spaces at left
+    (r'[ \t]+',                        None),  # ignore spaces at left
     (r'//[^\n]*',          None),
-    # (r'/\*[^\n]*[ \t]*[//]*[\w\W]*\*/',     None), 
+    # (r'/\*[^\n]*[ \t]*[//]*[\w\W]*\*/',     None),
     (r'/\*[^\n]+[ \t]*[//]*[\w\W]*[$\n]*\*/',     None),
     # (r'[ \t]*[\n]+',                  "newline"), # ignore space+repeated \n or space
 
@@ -20,7 +20,7 @@ tokenCollection = [
     (r'\bvar\b',  "type"),
     (r'\blet\b',  "type"),
     (r'\bconst\b',  "const"),
-    
+
 
     # SYNTAX
     (r'\bfunction\b',    "function"),
@@ -57,58 +57,60 @@ tokenCollection = [
 
     # ESCAPE SEQUENCE
     (r'\n',              "newline"),
-    (r'\(',              "kbki"), # kurung biasa kiri
-    (r'\)',              "kbka"), # kurung biasa kanan
-    (r'\{',              "kkki"), # kurung kurawal kiri
-    (r'\}',              "kkka"), # kurung kurawal kanan
-    (r'\[',              "kski"), # kurung siku kiri
-    (r'\]',              "kska"), # kurung siku kanan
-    (r'\:',              "td"), # titik dua
-    (r'\;',              "tk"), # titik koma
-    (r'\,',              "km"), # koma
+    (r'\(',              "kbki"),  # kurung biasa kiri
+    (r'\)',              "kbka"),  # kurung biasa kanan
+    (r'\{',              "kkki"),  # kurung kurawal kiri
+    (r'\}',              "kkka"),  # kurung kurawal kanan
+    (r'\[',              "kski"),  # kurung siku kiri
+    (r'\]',              "kska"),  # kurung siku kanan
+    (r'\:',              "td"),  # titik dua
+    (r'\;',              "tk"),  # titik koma
+    (r'\,',              "km"),  # koma
 
 
     # Operator (boolean)
     (r'\===',            "isstricteq"),
-    (r'!==',             "notstricteq"), 
+    (r'!==',             "notstricteq"),
     (r'\==',             "iseq"),
-    (r'\!=',             "isneq"), 
+    (r'\!=',             "isneq"),
     (r'\<=',             "leq"),
-    (r'\<',              "l"), # less
-    (r'\>=',             "geq"), 
-    (r'\>',              "g"), # greater
-    (r'\&&',             "and"), # and boolean
-    (r'\||',             "or"), # or boolean
-    (r'\!',              "notb"), # not boolean
-    (r'\?',              "andb"), # ternary (dipasangkan dengan :) e.g. x = expr1 ? val1 : val2
+    (r'\<',              "l"),  # less
+    (r'\>=',             "geq"),
+    (r'\>',              "g"),  # greater
+    (r'\&&',             "and"),  # and boolean
+    (r'\||',             "or"),  # or boolean
+    (r'\!',              "notb"),  # not boolean
+    # ternary (dipasangkan dengan :) e.g. x = expr1 ? val1 : val2
+    (r'\?',              "andb"),
 
     # Operator (arithmetic)
-    (r'\=',              "is"), # assigment
-    (r'\+',              "sum"), # sum , concat str
+    (r'\=',              "is"),  # assigment
+    (r'\+',              "sum"),  # sum , concat str
     (r'\-',              "sub"),
     (r'\*',              "mul"),
     (r'\/',              "div"),
     (r'\%',              "mod"),
     (r'\**',             "pow"),
-    (r'\+=',             "peq"), # plus equal
-    (r'\-=',             "meq"), # minus equal
-    (r'\++',             "inc"), # increment
-    (r'\--',             "dec"), # decrement
+    (r'\+=',             "peq"),  # plus equal
+    (r'\-=',             "meq"),  # minus equal
+    (r'\++',             "inc"),  # increment
+    (r'\--',             "dec"),  # decrement
     (r'\^',              "xor"),
-    (r'\&',              "andb"), # and bitwise
-    (r'\|',              "orb"), # or bitwise
+    (r'\&',              "andb"),  # and bitwise
+    (r'\|',              "orb"),  # or bitwise
 
 
     # Var Name, Class method, Obj Props
     (r'[a-zA-Z_][a-zA-Z0-9_]*',             "id"),
-    (r'\w+[.]\w+',        "kartitik"), 
+    (r'\w+[.]\w+',        "kartitik"),
 ]
 
+
 def lexer(text, tokenCollection):
-    currentPos = 1 # position in current line
-    currentLine = 1 # current line
+    currentPos = 1  # position in current line
+    currentLine = 1  # current line
     usedTokens = []
-    pointerText = 0 # text pointer
+    pointerText = 0  # text pointer
     while(pointerText < len(text)):
         if text[pointerText] == '\n':
             currentLine += 1
@@ -117,26 +119,30 @@ def lexer(text, tokenCollection):
         match = None
 
         for tokenExpr in tokenCollection:
-            pattern , tokenTag = tokenExpr
+            pattern, tokenTag = tokenExpr
 
-            regex = re.compile(pattern) # init regex
-            match = regex.match(text, pointerText) # check till current pointer
+            regex = re.compile(pattern)  # init regex
+            # check till current pointer
+            match = regex.match(text, pointerText)
 
-            if match: # check matcher
-                if tokenTag: # get token
+            if match:  # check matcher
+                if tokenTag:  # get token
                     currentTokens = tokenTag
                     usedTokens.append(currentTokens)
                 break
 
-        if not match: # throws error if not match
-            print(f"\nSyntax Error!\nLine: {currentLine},{currentPos} (character: {text[pointerText]})")
+        if not match:  # throws error if not match
+            print(
+                f"\nSyntax Error!\nLine: {currentLine},{currentPos} (character: {text[pointerText]})")
             sys.exit(1)
-        else: # set current text pointer to end of current match position
-            pointerText = match.end(0) # set pointer to end of current match position
+        else:  # set current text pointer to end of current match position
+            # set pointer to end of current match position
+            pointerText = match.end(0)
 
         currentPos += 1
     print(usedTokens)
     return usedTokens
+
 
 def parseTextToken(path):
     file = open(path)
@@ -150,5 +156,5 @@ def parseTextToken(path):
 
     return tokenInText
 
-parseTextToken('TC1.txt')
 
+parseTextToken('TC1.txt')
--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import TXTtoCFG
 import CFGtoCNF
+import CFG
+import CYK
 
-cfg = TXTtoCFG.parseCFG('testgrammar3.txt')
 
-cfg = CFGtoCNF.CFG(cfg)
-cfg.CFGtoCNF()
+# cfg = TXTtoCFG.parseCFG('testgrammar3.txt')
+
+# cfg = CFGtoCNF.CFG(cfg)
+# cfg.CFGtoCNF()
+
+# CFG = CFG.CFG()
+grammar = CFG.Grammar().parseCFG('testgrammar3.txt')
+
+for x in grammar:
+    print(x)
+
+lexer = ['a', 'b']
+CYK.cykParse(lexer, grammar)
