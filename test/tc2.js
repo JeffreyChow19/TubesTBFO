7e46a6b7e63768be616be4b6bf78d41b6ecbdde8
Jeffrey Chow <16521353@mahasiswa.itb.ac.id>
2022-11-24 23:03:58 +0700

    added 3 testcase

---
+++ test/tc2.js
@@ -0,0 +1,9 @@
+const temp = "jimly";
+switch (temp) {
+  case "jimly":
+    console.log("hello");
+  case "firdaus":
+    console.log("world");
+  default:
+    break;
+}
---
+++ test/tc3.js
@@ -0,0 +1,9 @@
+try {
+  if (winson[-1] != true) {
+    console.log("yey");
+  }
+} catch (err) {
+  console.log("error ni bro...");
+} finally {
+  console.log("you have passed the last stage...");
+}
