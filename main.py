cfa72891b0253c8ca003c43b1785f8c6045e44d1
Jeffrey Chow <16521353@mahasiswa.itb.ac.id>
2022-11-25 00:56:49 +0700

    fix: coloring and splash screen

--- main.py
+++ main.py
@@ -23,29 +23,38 @@ try:
     parser.add_argument('file', type=argparse.FileType('r'))
     args = parser.parse_args()
     filename = args.file.name
+
+    print(Fore.BLUE +
+          f'Checking {filename}...\nPlease wait for a moment...' + Style.RESET_ALL)
+
+    start = time.time()
+    # grammar
+    grammar = CFG.Grammar().parseCFG('grammar.txt')
+
+    # lexer
+    lexer = lexer.parseToToken(filename)
+
+    # CYK algorithm
+    result = CYK.cykParse(lexer, grammar)
+
+    end = time.time()
+
+    # Result
+    if result:
+        print(Fore.GREEN)
+        print("|--------------------|")
+        print("| Compile Success!!! |")
+        print("|--------------------|")
+        print(Style.RESET_ALL)
+    else:
+        print(Fore.RED)
+        print("|------------------|")
+        print("| Compile Error... |")
+        print("|------------------|")
+        print(Style.RESET_ALL)
+
+    print(Fore.YELLOW +
+          f'Execution time : {(end-start):.2f} seconds\n', Style.RESET_ALL)
+
 except:
     print(Fore.RED + "\nFile not found :)\n" + Style.RESET_ALL)
-
-# grammar
-grammar = CFG.Grammar().parseCFG('grammar.txt')
-
-# lexer
-lexer = lexer.parseToToken(filename)
-
-# CYK algorithm
-result = CYK.cykParse(lexer, grammar)
-
-# Result
-if result:
-    print(Fore.GREEN)
-    print("|--------------------|")
-    print("| Compile Success!!! |")
-    print("|--------------------|")
-    print(Style.RESET_ALL)
-else:
-    print(Fore.RED)
-    print("|------------------|")
-    print("| Compile Error... |")
-    print("|------------------|")
-    print(Style.RESET_ALL)
-print(result)
