c6ac7c99752e66465806a73ebcf77709cedcc3dc
Jeffrey Chow <16521353@mahasiswa.itb.ac.id>
2022-11-24 23:19:01 +0700

    added testcase

--- test/tc5.js
+++ test/tc5.js
@@ -1,6 +1,8 @@
 function abjad() {
   let a = b + c;
   console.log(a);
+
+  return a;
 }
 
 export default abjad;
---
+++ test/tc8.js
@@ -0,0 +1,6 @@
+let a = true;
+var b = false;
+const c = true;
+const d = false;
+const e = NULL;
+throw "this is an error...";
