787d4477c210a8e69a28c533fd1ce9c0b67e6419
Tansil011019 <16521295@std.stei.itb.ac.id>
2022-11-25 17:34:36 +0700

    feat: testcase 12

---
+++ test/tc12.js
@@ -0,0 +1,18 @@
+// inputAcc.js
+function do_something(x) {
+    // This is a sample comment
+    if (x == 0) {
+      return 0;
+    } else if (x + 4 == 1) {
+      if (true) {
+        return 3;
+      } else {
+        return 2;
+      }
+    } else if (x == 32) {
+      return 4;
+    } else {
+      return "Momen";
+    }
+  }
+  
