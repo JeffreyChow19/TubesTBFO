93ac047620b6ad4ae3ff6b0dd1eb1c4302b486ad
Jeffrey Chow <16521353@mahasiswa.itb.ac.id>
2022-11-25 08:58:13 +0700

    fix: main

--- main.py
+++ main.py
@@ -3,31 +3,32 @@ import CYK
 import lexer
 import argparse
 import time
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
 
 
 def splash():
     print(Fore.YELLOW)
-    print("============")
-    print("三位棉兰男神")
-    print("============")
+    print("============================")
+    print("  JAVASCRIPT SYNTAX LINTER")
+    print("        三位棉兰男神        ")
+    print("============================")
     print(Style.RESET_ALL)
 
 
-# splash screen
-splash()
-
 # argparse
-# try:
 parser = argparse.ArgumentParser()
 parser.add_argument('file', type=argparse.FileType('r'))
 args = parser.parse_args()
 filename = args.file.name
 
+# splash
+splash()
 print(Fore.BLUE +
-        f'Checking {filename}...\nPlease wait for a moment...' + Style.RESET_ALL)
+      f'Checking {filename}...\nPlease wait for a moment...' + Style.RESET_ALL)
 
+# timer start
 start = time.time()
+
 # grammar
 grammar = CFG.Grammar().parseCFG('grammar.txt')
 
@@ -37,6 +38,7 @@ lexer = lexer.parseToToken(filename)
 # CYK algorithm
 result = CYK.cykParse(lexer, grammar)
 
+# time end
 end = time.time()
 
 # Result
@@ -53,8 +55,6 @@ else:
     print("|------------------|")
     print(Style.RESET_ALL)
 
+# execution time
 print(Fore.YELLOW +
-        f'Execution time : {(end-start):.2f} seconds\n', Style.RESET_ALL)
-
-# except:
-#     print(Fore.RED + "\nFile not found :)\n" + Style.RESET_ALL)
+      f'Execution time : {(end-start):.2f} seconds\n', Style.RESET_ALL)
